e1af475350cf28509ace96ed428409f540de3c89
sirdjay <sirdjay@yahoo.com>
2022-04-04 15:15:07 +0100

    instance count changed

--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 module "spin_up_machine" {
     source = "./mymodules"
-    instance_count = 0
+    instance_count = 2
     modulescripts = ["scripts/apache.sh","scripts/tomcat.sh"]
     node_name = ["web server","App Server"]
     environment = "dev"
